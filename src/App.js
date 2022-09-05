25bb9ea6430262a5e834e7d03c01c9e635d3c114
Boriwan <boris.bocek@educanet.com>
2022-09-05 13:09:50 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <div id='info'>
+        <h1>boris boček</h1>
+        <p>Hi this is my portfolio page. Ad aliquip dolore cupidatat laboris eu amet laboris ea mollit non consectetur eiusmod. Voluptate incididunt commodo ipsum id fugiat et ipsum laborum ipsum non ad elit amet Lorem. Laborum consectetur cupidatat sunt ex nisi qui ullamco mollit et adipisicing. Sunt labore dolor enim mollit deserunt ea anim aute labore tempor voluptate eiusmod fugiat.</p>
+      </div>
+
+      <div id='skills'>
+        <h2>Skills</h2>
+        <h4>Lagnuages:</h4>
+        <h4>Frameworks:</h4>
+        <h4>Programms:</h4>
+      </div>
+
+      <div id='socials'>
+        <h2>Socials</h2>
+      </div>
+    </>
+
   );
 }
 
