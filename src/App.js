0f58d6bcdfd52ad15d26a3297e0140a96b4fdf50
Boriwan <boris.bocek@educanet.com>
2022-09-07 00:16:42 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import './App.css';
 
 function App() {
   return (
     <>
       <div id='info'>
-        <h1>boris boček</h1>
+        <h1>Boris Boček</h1>
         <p>Hi this is my portfolio page. Ad aliquip dolore cupidatat laboris eu amet laboris ea mollit non consectetur eiusmod. Voluptate incididunt commodo ipsum id fugiat et ipsum laborum ipsum non ad elit amet Lorem. Laborum consectetur cupidatat sunt ex nisi qui ullamco mollit et adipisicing. Sunt labore dolor enim mollit deserunt ea anim aute labore tempor voluptate eiusmod fugiat.</p>
       </div>
 
-      <div id='skills'>
-        <h2>Skills</h2>
-        <h4>Lagnuages:</h4>
-        <h4>Frameworks:</h4>
-        <h4>Programms:</h4>
+      <h2>Skills</h2>
+
+      <h4>Lagnuages</h4>
+      <div className="d-flex flex-row justify-content-center flex-wrap">
+       
+        <div className="card">
+          <div className="card-body">
+            <h5>Java</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-body">
+            <h5>JavaScript</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-body">
+            <h5>TypeScript</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-body">
+            <h5>Python</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-body">
+            <h5>C++</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
       </div>
 
-      <div id='socials'>
+
+      <h4>Frameworks</h4>
+      <h4>Programms</h4>
+
+      <div>
         <h2>Socials</h2>
       </div>
     </>
