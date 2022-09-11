c765cf87c6fafc29922774857ac46d0f408601ef
Boriwan <boris.bocek@educanet.com>
2022-09-11 16:46:18 +0200

    progress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <div className="container mt-2 text-center">
+            <div className="container mt-5 ">
                 <h1>Hi Welcome To My Portfolio</h1>
 
                 <p>Cillum occaecat non ullamco sit fugiat id reprehenderit do pariatur elit est duis veniam laborum. Laborum duis sit aute deserunt non commodo irure. Irure velit nulla aute et fugiat eiusmod. Deserunt esse aliqua qui consectetur excepteur labore proident eiusmod dolore ut. Veniam consequat cupidatat deserunt dolore esse fugiat labore quis sit mollit ea cupidatat.</p>
